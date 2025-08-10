ace5e099c4aa05a0b53fb018f33a4a429405545b
JoaoVitorSales <picoletando@gmail.com>
2025-08-09 23:41:23 -0300

    feat: adiciona rotas de Delete, Put e Read

--- src/domains/usuario/controllers/index.ts
+++ src/domains/usuario/controllers/index.ts
@@ -1,5 +1,5 @@
 import {Router, Request, Response, NextFunction} from "express";
-import { readUsuarios } from "../services/UserServices"
+import { readUsuarios, readUsuariosByID, createUsuario, updateUsuario, deleteUsuario } from "../services/UserServices"
 
 const router = Router();
 
@@ -12,5 +12,42 @@ router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     }
 }
 );
+
+router.get("/:id", async(req: Request, res: Response, next: NextFunction) => {
+    try{
+        const usuarioUnico = await readUsuariosByID(Number(req.params.id));
+        res.json(usuarioUnico);
+    }catch(error){
+        next(error);
+    }
+});
+
+router.post("/", async(req: Request, res: Response, next: NextFunction) => {
+    try{
+        const usuario = await createUsuario(req.body);
+        res.json(`usuario com nome ${usuario.nome} criado`);
+    }catch(error){
+        next(error);
+    }
+});
+
+router.put("/:id", async(req: Request, res: Response, next: NextFunction) => {
+    try{
+        const usuario = await updateUsuario(Number(req.params.id), req.body);
+        res.json(`usuario com nome ${usuario.nome} atualizado`);
+    }catch(error){
+        next(error);
+    }
+});
+
+router.delete("/:id", async(req: Request, res: Response, next: NextFunction) => {
+    try{
+        const usuario = await deleteUsuario(Number(req.params.id));
+        res.json(`usuario com nome ${usuario.nome} deletado`);
+    }catch(error){
+        next(error);
+    }
+});
+
 
 export default router;
