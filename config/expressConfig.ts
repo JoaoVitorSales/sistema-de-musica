0d9ed08b8cb35e0f4e0616d152f7ca1391590406
JoaoVitorSales <picoletando@gmail.com>
2025-08-16 01:47:17 -0300

    feat: adiciona os services da tabela userMusic

--- config/expressConfig.ts
+++ config/expressConfig.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import express, { Express } from "express";
 import usuarioRoutes from "../src/domains/user/controllers";
 import musicaRoutes from "../src/domains/music/controllers";
+import artistRoutes from "../src/domains/artist/controllers";
+import UserMusicRoutes from "../src/domains/userMusic/controllers";
 
 
 dotenv.config()
@@ -20,4 +22,6 @@ app.use(express.urlencoded({
     extended: true
 }))
 app.use("/api/usuario", usuarioRoutes)
+app.use("/api/artista", artistRoutes)
 app.use("/api/musica", musicaRoutes)
+app.use("/api/listagem", UserMusicRoutes)
